7030abce5b3f1f750ccf964bcfd845a48d18023f
Patrick <patty6339@gmail.com>
2025-02-08 18:34:33 +0300

    working files

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region = "us-east-1" # Change to your preferred AWS region
 }
 
+# Create a key pair for SSH access
+resource "aws_key_pair" "flask_key" {
+  key_name   = "flask-key-pair"          # Name of the key pair
+  public_key = file("~/.ssh/id_rsa.pub") # Path to your public key file
+}
+
+# Create a security group for the Flask API EC2 instance
 resource "aws_security_group" "flask_sg" {
   name        = "flask_security_group"
   description = "Security group for Flask API EC2 instance"
@@ -42,12 +49,19 @@ resource "aws_security_group" "flask_sg" {
   }
 }
 
+# Create the EC2 instance
 resource "aws_instance" "flask_ec2" {
-  ami             = "ami-04b4f1a9cf54c11d0" # Change to the latest Amazon Linux AMI or your preferred AMI
+  ami             = "ami-04b4f1a9cf54c11d0" # Ubuntu 20.04 LTS AMI in us-east-1 (change as needed)
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.flask_sg.name]
+  key_name        = aws_key_pair.flask_key.key_name # Associate the key pair with the instance
 
   tags = {
     Name = "Flask-API-Server"
   }
+}
+
+# Output the public IP of the instance
+output "instance_public_ip" {
+  value = aws_instance.flask_ec2.public_ip
 }
